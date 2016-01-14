46897bb6ca184ba99c7b159591b003e7c78a6b14
Emily MaRee <lee@mleemaree.com>
2016-01-14 12:22:18 +0100

    separated classes

--- thursday.rb
+++ thursday.rb
@@ -1,30 +1,37 @@
 require 'pry'
 
 class User_Login
-	attr_reader :user, :password
+	attr_reader :user, :password, :logged
 	def initialize(user, password)
 		@users = {:user => "lee", :password => "1234"}
 		@user = user
 		@password = password
+		@logged = logged
 		validate
 	end
 
 	def validate
 		if @user == @users[:user] && @password == @users[:password]
-
-			logged_in
+			@logged = true
 		end
 	end
 
-	def logged_in
-		if @user == "lee" && @password == "1234"
+end
+
+class Word_Count
+	def initialize(user)
+		@user = user
+		count
+	end
+	def count
+		if @user.logged
 			puts "please enter some text"
 			text= gets.chomp
 			puts "The length of your text is #{text.length} characters."
 		end
 	end
-
 end
+
 
 
 
@@ -38,4 +45,6 @@ users = {:user => user, :password => password}
 
 
 user1 = User_Login.new(users[:user], users[:password])
+
+Word_Count.new(user1)
 
